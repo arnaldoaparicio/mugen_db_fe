08c3519d0127f60fbbf33d4fdda7c227b3744302
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-21 21:56:39 -0400

    Fix indentation

--- app/services/character_service.rb
+++ app/services/character_service.rb
@@ -1,10 +1,10 @@
 class CharacterService
-    def self.all_origins
-        response = connection.get('/api/v1/characters')
-        json = JSON.parse(response.body, symbolize_names: true)
-    end
+  def self.all_origins
+    response = connection.get('/api/v1/characters')
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
 
-      def self.connection
-        Faraday.new(url: 'http://localhost:5000')
-    end
+  def self.connection
+    Faraday.new(url: 'http://localhost:5000')
+  end
 end
