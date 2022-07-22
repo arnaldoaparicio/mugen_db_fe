fd74f1ca4e372211a7d42e23bf9892374b0d6185
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-22 17:35:32 -0400

    Remove connection method

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -1,10 +1,5 @@
 class FightersController < ApplicationController
-  def connection
-    Faraday.new(url: 'http://localhost:5000')
-  end
-
   def index
     @fighter_variants = FighterFacade.variants(params[:id], params[:fighter_id])
-  
   end
 end
