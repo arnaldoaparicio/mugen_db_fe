6c293907981f541b3b770707efd63be7e000b3ff
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-22 17:11:34 -0400

    Remove Faraday connection

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,8 +1,4 @@
 class CharactersController < ApplicationController
-  def connection
-    Faraday.new(url: 'http://localhost:5000')
-  end
-
   def index
     @characters = CharacterFacade.origins
   end
