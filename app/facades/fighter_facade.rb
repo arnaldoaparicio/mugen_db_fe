c601c7d3786f3d8a591ac457d8978e411310c5c8
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-22 17:34:39 -0400

    Remove unused code

--- app/facades/fighter_facade.rb
+++ app/facades/fighter_facade.rb
@@ -1,11 +1,8 @@
 class FighterFacade
-
   def self.variants(series_id, fighter_id)
-    # response = connection.get("/api/v1/characters/#{params[:id]}/#{params[:fighter_id]}")
     json = FighterService.all_variants(series_id, fighter_id)
     @fighter_variants = json[:data].map do |variant_data|
       FighterVariant.new(variant_data)
     end
   end
-
 end
