910a8f94c3700fe15dc29f11eda53cadce130261
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-21 22:15:00 -0400

    Add all_fighters method

--- app/services/character_service.rb
+++ app/services/character_service.rb
@@ -4,6 +4,11 @@ class CharacterService
     json = JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.all_fighters
+    response = connection.get("/api/v1/characters/#{params[:id]}")
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.connection
     Faraday.new(url: 'http://localhost:5000')
   end
