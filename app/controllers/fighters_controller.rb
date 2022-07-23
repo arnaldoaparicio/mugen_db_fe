5f7f57e1ae41bdb37012d3446df26c501c44c7f3
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-22 22:03:12 -0400

    Add single_fighter and single_game_origin variables

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -1,5 +1,12 @@
 class FightersController < ApplicationController
   def index
+    fighters = CharacterFacade.fighters(params[:id])
+    characters = CharacterFacade.origins
+
+    @single_game_origin = characters.find{ |origin| origin.id == params[:id] }
+    @single_fighter = fighters.find{ |fighter| fighter.id == params[:fighter_id] }
+    
     @fighter_variants = FighterFacade.variants(params[:id], params[:fighter_id])
+    @character = CharacterFacade.origins
   end
 end
