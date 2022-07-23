240224c239219d304fa5d196f5a6661a5d51adfb
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-23 16:52:47 -0400

    Modify create method

--- app/controllers/fighter_variants_controller.rb
+++ app/controllers/fighter_variants_controller.rb
@@ -1,8 +1,12 @@
 class FighterVariantsController < ApplicationController
   def new
-    require "pry"; binding.pry
   end
 
   def create
+    require "pry"; binding.pry
+    fighter_variant = FighterVariantFacade.create_fighter_variant(name: params[:name],
+    author: params[:author], website: params[:website], game_name: params[game_name],
+    fighter_id: params[:fighter_id])
+    redirect to "/characters/#{params[:id]}/#{params[:fighter_id]}"
   end
 end
