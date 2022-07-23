36d9e30d29472a51f73b57aa7b25c7b10ca0af2f
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-22 22:04:38 -0400

    Convert global variable to local variable

--- app/facades/fighter_facade.rb
+++ app/facades/fighter_facade.rb
@@ -1,7 +1,7 @@
 class FighterFacade
   def self.variants(series_id, fighter_id)
     json = FighterService.all_variants(series_id, fighter_id)
-    @fighter_variants = json[:data].map do |variant_data|
+    fighter_variants = json[:data].map do |variant_data|
       FighterVariant.new(variant_data)
     end
   end
