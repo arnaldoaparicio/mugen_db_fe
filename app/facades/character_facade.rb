4f83a3a7b4d858b726451f149c1db5bc301c2ee7
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-22 22:04:06 -0400

    Convert global variable into local variable

--- app/facades/character_facade.rb
+++ app/facades/character_facade.rb
@@ -1,14 +1,14 @@
 class CharacterFacade
   def self.origins
     json = CharacterService.all_origins
-    @characters = json[:data].map do |character_data|
+    characters = json[:data].map do |character_data|
       Character.new(character_data)
     end
   end
 
   def self.fighters(character_id)
     json = CharacterService.all_fighters(character_id)
-    @fighters = json[:data].map do |fighter_data|
+    fighters = json[:data].map do |fighter_data|
       Fighter.new(fighter_data)
     end
   end
