41a37c813c68019c3f30c3b9a39aafc125f32c5c
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-23 17:23:58 -0400

    Add crete_variant method

--- app/facades/fighter_variant_facade.rb
+++ app/facades/fighter_variant_facade.rb
@@ -1,6 +1,7 @@
 class FighterVariantFacade
-
-def self.create_fighter_variant
-end
-
+  def self.create_fighter_variant(data)
+    # require "pry"; binding.pry
+    fighter_variant = FighterVariantService.create_new_fighter_variant(data)[:data]
+    FighterVariant.new(fighter_variant)
+  end
 end
