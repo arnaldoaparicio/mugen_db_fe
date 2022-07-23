72021b1d53eff786a59acb2a6ee84cd1051a1437
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-23 17:23:12 -0400

    Fix redirect

--- app/controllers/fighter_variants_controller.rb
+++ app/controllers/fighter_variants_controller.rb
@@ -3,10 +3,9 @@ class FighterVariantsController < ApplicationController
   end
 
   def create
-    require "pry"; binding.pry
     fighter_variant = FighterVariantFacade.create_fighter_variant(name: params[:name],
-    author: params[:author], website: params[:website], game_name: params[game_name],
+    author: params[:author], website: params[:website], game_name: params[:game_name],
     fighter_id: params[:fighter_id])
-    redirect to "/characters/#{params[:id]}/#{params[:fighter_id]}"
+    redirect_to "/characters/#{params[:id]}/#{params[:fighter_id]}"
   end
 end
