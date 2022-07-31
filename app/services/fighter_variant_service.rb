023c1378b74f23c222f12efeb429fb626afedb88
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-07-31 17:13:58 -0400

    Remove unused code

--- app/services/fighter_variant_service.rb
+++ app/services/fighter_variant_service.rb
@@ -1,19 +1,15 @@
 class FighterVariantService
   def self.connection
-    Faraday.new(url: 'http://localhost:5000') do |conn|
-  # POST/PUT params encoders:
-  conn.request :multipart
-  conn.request :url_encoded
-
-  # Last middleware must be the adapter:
-  conn.adapter :net_http
-  # fighter_variant[:avatar] = Faraday::UploadIO.new(fighter_variant[:avatar].tempfile.path, 'image/png')
-end
+    Faraday.new(url: 'http://localhost:5000') do |f|
+      f.request :multipart
+      f.request :url_encoded
+      f.adapter :net_http
+    end
   end
 
   def self.create_new_fighter_variant(fighter_variant)
     fighter_variant[:avatar] = Faraday::Multipart::FilePart.new(fighter_variant[:avatar].tempfile.path, 'image/png')
-    response = connection.post('/api/v1/fighter_variants', fighter_variant ) do |req|
+    response = connection.post('/api/v1/fighter_variants', fighter_variant) do |req|
       req.body = { fighter_variant: fighter_variant }
     end
     JSON.parse(response.body, symbolize_names: true)
