30530fda3ea0871f7cb51a5709ef8c6afc52e856
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-08-10 21:13:48 -0400

    Add create method

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -6,4 +6,9 @@ class CharactersController < ApplicationController
   def show
     @fighters = CharacterFacade.fighters(params[:id])
   end
+
+  def create
+    character = CharacterFacade.create_character(origin: params[:origin])
+    redirect_to '/characters'
+  end
 end
