cf1513221d8fe939f7ec8e3a0c6a987ebc07cf97
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-08-10 21:14:12 -0400

    Add post route to create new character

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get '/characters', to: 'characters#index'
+  get '/characters/new', to: 'characters#new'
   get '/characters/:id', to: 'characters#show'
   get '/characters/:id/:fighter_id', to: 'fighters#index'
   get '/characters/:id/:fighter_id/new', to: 'fighter_variants#new'
 
   post '/fighter_variants', to: 'fighter_variants#create'
+  post '/characters', to: 'characters#create'
 
 end
