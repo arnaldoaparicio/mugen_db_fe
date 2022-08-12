b266c87d54189431c9f1da56f1c29c9701b57ddb
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-08-12 18:37:50 -0400

    Add route to create fighter

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   get '/characters', to: 'characters#index'
   get '/characters/new', to: 'characters#new'
   get '/characters/:id', to: 'characters#show'
+  get '/characters/:id/new', to: 'fighters#new'
   get '/characters/:id/:fighter_id', to: 'fighters#index'
   get '/characters/:id/:fighter_id/new', to: 'fighter_variants#new'
 
   post '/fighter_variants', to: 'fighter_variants#create'
   post '/characters', to: 'characters#create'
+  post '/fighters', to: 'fighters#create'
 
 end
