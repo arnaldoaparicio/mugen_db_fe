f5806417ad6a24383c6798b35ae5de8bd584ae65
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-08-12 18:42:15 -0400

    Add create_new_fighter method

--- app/services/fighter_service.rb
+++ app/services/fighter_service.rb
@@ -5,6 +5,14 @@ class FighterService
   end
 
   def self.connection
-    Faraday.new(url: 'https://boiling-peak-48685.herokuapp.com')
+    # Faraday.new(url: 'https://boiling-peak-48685.herokuapp.com')
+    Faraday.new(url: 'http://localhost:5000')
+  end
+
+  def self.create_new_fighter(fighter)
+    response = connection.post("/api/v1/fighters", fighter) do |req|
+      req.body = { fighter: fighter }
+    end
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
