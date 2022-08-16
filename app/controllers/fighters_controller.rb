5dd9b1fdee5a4f31b9a1205896b7371e0c1b4b78
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-08-16 09:58:45 -0400

    Fix spacing

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -3,15 +3,14 @@ class FightersController < ApplicationController
     fighters = CharacterFacade.fighters(params[:id])
     characters = CharacterFacade.origins
 
-    @single_game_origin = characters.find{ |origin| origin.id == params[:id] }
-    @single_fighter = fighters.find{ |fighter| fighter.id == params[:fighter_id] }
-    
+    @single_game_origin = characters.find { |origin| origin.id == params[:id] }
+    @single_fighter = fighters.find { |fighter| fighter.id == params[:fighter_id] }
+
     @fighter_variants = FighterFacade.variants(params[:id], params[:fighter_id])
     @character = CharacterFacade.origins
   end
 
   def create
-    # binding.pry
     fighter = FighterFacade.create_fighter(name: params[:name], character_id: params[:character_id])
     redirect_to "/characters/#{params[:character_id]}"
   end
