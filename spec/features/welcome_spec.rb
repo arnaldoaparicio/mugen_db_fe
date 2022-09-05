3813e715dad8d6e8658bd30d78e7a56cd8b67709
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-09-05 13:50:12 -0400

    Add tests to verify existence of links

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'welcome/index', type: :feature do
-  before(:each) do
-    visit('/')
-  end
-
   it 'has the welcome greeting' do
+    visit('/')
     expect(page).to have_content('Welcome to the MUGEN database.')
   end
 
   it 'has a link to characters page' do
-    
+    visit('/')
+    expect(page).to have_link('Characters')
+  end
+
+  it 'clicks on the Characters link' do
+    visit('/')
+    click_link('Characters')
+    expect(current_path).to eq('/characters')
   end
 end
