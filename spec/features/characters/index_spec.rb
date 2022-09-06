ef48fc2ee8dbde2bb2b79062d9d2e7838df52b29
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-09-06 19:43:18 -0400

    Add test to show contents within character origin page

--- spec/features/characters/index_spec.rb
+++ spec/features/characters/index_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe 'characters/index', type: :feature do
   it 'has some character origins' do
     visit('/characters')
-    save_and_open_page
     expect(page).to have_content("Arcana Heart")
     expect(page).to have_content('Marvel vs Series/X-COTA')
     expect(page).to have_content('Street Fighter 3')
@@ -12,5 +11,15 @@ RSpec.describe 'characters/index', type: :feature do
     expect(page).to have_content("JoJo's Bizarre Adventure")
     expect(page).to have_content('The King of Fighters')
     expect(page).to have_content('Street Fighter Alpha')
+  end
+
+  it 'shows the characters within the Darkstalkers/Vampire Savior character origin page' do
+    visit('/characters')
+
+    click_link 'Darkstalkers/Vampire Savior'
+
+    expect(page).to have_content('Lilith')
+    expect(page).to have_content('Jedah')
+    expect(page).to have_link('Add a new fighter')
   end
 end
