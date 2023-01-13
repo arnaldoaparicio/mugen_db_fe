e3dda5fe4a26e7c6f2e4adf51f9fed13328c14a8
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-13 10:23:50 -0500

    Remove attribute

--- app/poros/fighter_variant.rb
+++ app/poros/fighter_variant.rb
@@ -1,5 +1,5 @@
 class FighterVariant
-  attr_reader :id, :name, :author, :website, :game_name, :image, :avatar, :image_url
+  attr_reader :id, :name, :author, :website, :game_name, :avatar, :image_url
 
   def initialize(variant_data)
     @id = variant_data[:id]
@@ -7,7 +7,6 @@ class FighterVariant
     @author = variant_data[:attributes][:author]
     @website = variant_data[:attributes][:website]
     @game_name = variant_data[:attributes][:game_name]
-    @image = variant_data[:attributes][:image]
     @avatar = variant_data[:attributes][:avatar]
     @image_url = variant_data[:attributes][:image_url]
   end
