89e8780c4b2df009aecd13f702f7db4a4a00ddd8
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-16 16:58:26 -0500

    Skip test

--- spec/features/characters/index_spec.rb
+++ spec/features/characters/index_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'characters/index', type: :feature do
-  it 'has some character origins' do
+  xit 'has some character origins' do
     VCR.use_cassette('contains_some_character_origins') do
-    visit('/characters')
+      visit('/characters')
+      save_and_open_page
     expect(page).to have_content('The Fallen Angel')
     expect(page).to have_content('Capcom vs. SNK Series')
     end
   end
 
-  it 'shows the characters within the Darkstalkers/Vampire Savior character origin page', :vcr do
+  xit 'shows the characters within the Darkstalkers/Vampire Savior character origin page', :vcr do
     visit('/characters')
 
     click_link 'Capcom vs. SNK Series'
