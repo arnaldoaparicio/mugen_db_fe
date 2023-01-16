c2135a95471a795cfc1351fb11da6c722e4e045f
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-16 15:36:19 -0500

    Add more to test

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -10,5 +10,15 @@ RSpec.describe UserService do
     response = UserService.create_user(data)
 
     expect(response).to be_a Hash
+    expect(response).to have_key :data
+
+    expect(response[:data]).to have_key :id
+    expect(response[:data][:id]).to be_a String
+
+    expect(response[:data][:attributes]).to have_key :email
+    expect(response[:data][:attributes][:email]).to be_a String
+
+    expect(response[:data][:attributes]).to have_key :admin
+    
   end
 end
