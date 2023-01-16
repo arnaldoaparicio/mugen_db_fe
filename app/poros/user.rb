5012446d4dd794c83b1e0828301789ef98f2bf8f
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-16 15:06:39 -0500

    Fix spacing

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -1,2 +1,9 @@
 class User
-    e
+  attr_reader :id, :email, :admin
+
+  def initialize(user_data)
+    @id = user_data[:id]
+    @email = user_data[:attributes][:email]
+    @admin = user_data[:attributes][:admin]
+  end
+end
