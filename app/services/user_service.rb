3d0ef5a6e4d1a8269f92b90e6cc7b9504941be71
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-17 18:13:58 -0500

    Add get_user method

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -6,6 +6,11 @@ class UserService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.get_user(user_id)
+    response = connection.get("/api/v1/users/#{user_id}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.connection
     Faraday.new(url: 'http://localhost:3000')
   end
