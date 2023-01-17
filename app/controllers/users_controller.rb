28a0c170fca41284238de6fceb4684b5e2488182
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-16 22:05:45 -0500

    Fix spacing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,13 @@
 class UsersController < ApplicationController
+  def create
+    user = UserFacade.new_user(email: params[:email],
+                               password: params[:password],
+                               password_confirmation: params[:password],
+                               admin: params[:admin])
+    session[:id] = user.id
+    redirect_to '/'
+  end
 
-    def create
-        user = UserFacade.new_user(email: params[:email], 
-            password: params[:password], 
-            password_confirmation: params[:password],
-            admin: params[:admin])
-        session[:id] = user.id
-        redirect_to '/'
-            require 'pry'; binding.pry
-    end
-
-    def new
-    end
+  def new
+  end
 end
