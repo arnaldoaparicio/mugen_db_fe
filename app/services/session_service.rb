def3ac36a505e0a54b64589b2873ee10ac6f5989
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-17 21:35:46 -0500

    Change parameter count

--- app/services/session_service.rb
+++ app/services/session_service.rb
@@ -1,11 +1,9 @@
 class SessionService
+  def self.found_user(user_data)
+    response = connection.post('/api/v1/login', user_data)
+  end
 
-
-    
-
-    def self.connection
-        Faraday.new('http://localhost:3000')
-    end
-
-
+  def self.connection
+    Faraday.new('http://localhost:3000')
+  end
 end
