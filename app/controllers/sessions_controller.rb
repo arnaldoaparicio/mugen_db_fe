23cf5b68ab295b634722396016890bd1c8c34991
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-17 21:21:13 -0500

    Modify parameters

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,9 @@ class SessionsController < ApplicationController
     def new
     end
 
+    def create
+        user = SessionFacade.user_login(params[:email], params[:password])
+        session[:user_id] = user.id
+        redirect_to '/'
+    end
 end
