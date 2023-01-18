9365bbbc7116323c3002b5c8b7858ad1bc94ce02
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-18 11:09:27 -0500

    Fix found_user method

--- app/services/session_service.rb
+++ app/services/session_service.rb
@@ -3,7 +3,7 @@ class SessionService
     response = connection.post('/api/v1/login', user_data) do |req|
       req.body = { user: user_data }
     end
-    JSON.parse(response.body, symbolize_names: true)
+    JSON.parse(response.body, symbolize_names: true)[:data]
   end
 
   def self.connection
