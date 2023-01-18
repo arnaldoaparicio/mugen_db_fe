9afe75676117ebdf69bbfcc914be4af114ba7469
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-17 20:43:08 -0500

    Add post route for session login

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 
 end
