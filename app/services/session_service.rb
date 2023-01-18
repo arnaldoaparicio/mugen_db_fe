277f7e2e5534578cede60f8ca375904db4517149
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-18 10:58:46 -0500

    Complete found_user method

--- app/services/session_service.rb
+++ app/services/session_service.rb
@@ -1,6 +1,9 @@
 class SessionService
   def self.found_user(user_data)
-    response = connection.post('/api/v1/login', user_data)
+    response = connection.post('/api/v1/login', user_data) do |req|
+      req.body = { user: user_data }
+    end
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.connection
