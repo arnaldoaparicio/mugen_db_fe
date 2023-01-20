5e57552ae12fc5f18546fe0a1506f0d35e3f991a
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-01-20 14:44:55 -0500

    Fix formatting

--- app/controllers/fighter_variants_controller.rb
+++ app/controllers/fighter_variants_controller.rb
@@ -3,7 +3,7 @@ class FighterVariantsController < ApplicationController
   end
 
   def create
-    fighter_variant = FighterVariantFacade.create_fighter_variant(name: params[:name],
+    FighterVariantFacade.create_fighter_variant(name: params[:name],
     author: params[:author], website: params[:website], game_name: params[:game_name],
     fighter_id: params[:fighter_id], avatar: params[:avatar])
     redirect_to "/characters/#{params[:id]}/#{params[:fighter_id]}"
