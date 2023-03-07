749244776a1c93cdf1a23d10d14356b1308d97c9
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-03-07 14:33:11 -0500

    Modify test expectations

--- spec/features/characters/index_spec.rb
+++ spec/features/characters/index_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'characters/index', type: :feature do
-  xit 'has some character origins' do
+  it 'has some character origins' do
     VCR.use_cassette('contains_some_character_origins') do
-      visit('/characters')
-      save_and_open_page
+    visit('/characters')
     expect(page).to have_content('The Fallen Angel')
     expect(page).to have_content('Capcom vs. SNK Series')
     end
   end
 
-  xit 'shows the characters within the Darkstalkers/Vampire Savior character origin page', :vcr do
+  it 'shows the characters within the Capcom vs. SNK Series character origin page', :vcr do
     visit('/characters')
 
     click_link 'Capcom vs. SNK Series'
@@ -18,6 +17,5 @@ RSpec.describe 'characters/index', type: :feature do
     expect(page).to have_content('Ryu')
     expect(page).to have_content('Kyo')
     expect(page).to have_content('Guile')
-    expect(page).to have_link('Add a new fighter')
   end
 end
