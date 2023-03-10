ad6efddaa6413cdd68c259811a4c572ea368963a
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-03-10 16:34:21 -0500

    Add test to check for link while logged out

--- spec/features/characters/show_spec.rb
+++ spec/features/characters/show_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'characters/show page', type: :feature do
-  it "shows empty section" do
-    origin = Character.new(id: '1', attributes: {origin: 'Marvel vs. Capcom'})
+  xit "does not show 'add a new fighter' link  while logged out", :vcr do
+    origin = Character.new({id: '10', attributes: {origin: 'Marvel vs. Capcom'}})
     visit("/characters/#{origin.id}")
 
-    expect(page).to have_content('This section is currently empty. Please check back soon')
+    expect(page).to have_content('Back to Characters page')
   end
 end
