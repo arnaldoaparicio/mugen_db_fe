10b96108c15f876eef92a796571066fe3aecca0f
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-04-09 08:47:26 -0400

    Paginate array of characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,10 +1,12 @@
 class CharactersController < ApplicationController
   def index
     @characters = CharacterFacade.origins
+    @paginate_characters = Kaminari.paginate_array(@characters).page(params[:page]).per(12)
   end
 
   def show
     @fighters = CharacterFacade.fighters(params[:id])
+    @paginate_fighters = Kaminari.paginate_array(@fighters).page(params[:page]).per(12)
 
     all_origins = CharacterFacade.origins
     @origin = all_origins.find {|origin| origin.id == params[:id]}
