2c94ac357b7112e23273dc8754791bb15a0c7248
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-05-25 11:57:09 -0400

    Add test to check for 'Edit origin' button

--- spec/features/characters/edit_spec.rb
+++ spec/features/characters/edit_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'characters/edit page', type: :feature do
-  it 'shows form to change origin name' do
+  it 'shows form to change origin name', :vcr do
     origin = Character.new( {id: '1', attributes: { origin: 'Marvel vs. Capcom'} } )
     visit("/characters/#{origin.id}")
 
+    expect(page).to have_button('Edit origin')
     
   end
 end
