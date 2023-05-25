5f612188e7f4a7e9bd7ac66d79ee43f3e9b69863
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-05-25 13:17:41 -0400

    Omit test

--- spec/features/characters/edit_spec.rb
+++ spec/features/characters/edit_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'characters/edit page', type: :feature do
-  it 'shows form to change origin name', :vcr do
-    origin = Character.new( {id: '1', attributes: { origin: 'Marvel vs. Capcom'} } )
-    visit("/characters/#{origin.id}")
+  xit 'shows form to change origin name', :vcr do
+    origin = Character.new( {id: '2', attributes: { origin: 'Capcom vs. SNK series'} } )
+    visit("/characters")
 
+    user = User.new({id: '1', attributes: { email: 'mailman@mail.com', admin: true } })
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     expect(page).to have_button('Edit origin')
     
+    first(:button, 'Edit origin')
+
+    expect(current_path).to eq("/characters/#{origin.id}/edit")
   end
 end
