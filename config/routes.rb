6f960cc0d631dc6dedb414d7ffdb3afe5aff220e
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-05-26 16:19:06 -0400

    Add edit route to 'fighters' controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
 
   post '/fighter_variants', to: 'fighter_variants#create'
   post '/characters', to: 'characters#create'
-  patch 'characters/:id/edit', to: 'characters#update'
+  patch '/characters/:id/edit', to: 'characters#update'
+  patch '/characters/:id/:fighter_id/edit', to: 'fighters#update'
   post '/fighters', to: 'fighters#create'
   # get '/register', to: 'users#new'
   # post '/register', to: 'users#create'
