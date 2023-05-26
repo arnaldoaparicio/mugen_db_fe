f796ce454c49e6e68c2cd3983332927915ae65f5
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-05-26 16:43:40 -0400

    Add update route to fighters controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   post '/fighter_variants', to: 'fighter_variants#create'
   post '/characters', to: 'characters#create'
   patch '/characters/:id/edit', to: 'characters#update'
+  patch '/characters/:id/:fighter_id', to: 'fighters#update'
   post '/fighters', to: 'fighters#create'
   # get '/register', to: 'users#new'
   # post '/register', to: 'users#create'
