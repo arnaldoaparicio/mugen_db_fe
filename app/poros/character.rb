6799b842dba3f3c94a242e2e32260256ea2a6a86
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-02-26 17:15:52 -0500

    Remove 'slug_url' from attr_reader and remove pry

--- app/poros/character.rb
+++ app/poros/character.rb
@@ -2,7 +2,7 @@ class Character
   attr_reader :id, :name, :name_tag
 
   def initialize(character_data)
-    @id = character_data[:id]
+    @id = character_data[:attributes][:slug_url]
     @name = character_data[:attributes][:origin]
     @name_tag = character_data[:attributes][:origin_tag]
   end
