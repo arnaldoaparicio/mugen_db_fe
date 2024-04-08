b499808c905c5ea72ead826fabf3679db951fd8b
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-04-08 09:48:49 -0400

    Replace id with slug_url

--- app/poros/fighter.rb
+++ app/poros/fighter.rb
@@ -2,7 +2,7 @@ class Fighter
   attr_reader :id, :name
 
   def initialize(fighter_data)
-    @id = fighter_data[:id]
+    @id = fighter_data[:attributes][:slug_url]
     @name = fighter_data[:attributes][:name]
   end
 end
