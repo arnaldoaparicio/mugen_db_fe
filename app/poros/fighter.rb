2919c0d6198ea6667c992daded90b5124cfedd28
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-04-08 09:52:55 -0400

    Add 'name_tag' attribute

--- app/poros/fighter.rb
+++ app/poros/fighter.rb
@@ -1,8 +1,9 @@
 class Fighter
-  attr_reader :id, :name
+  attr_reader :id, :name, :name_tag
 
   def initialize(fighter_data)
     @id = fighter_data[:attributes][:slug_url]
     @name = fighter_data[:attributes][:name]
+    @name_tag = fighter_data[:attributes][:name_tag]
   end
 end
